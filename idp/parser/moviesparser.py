6fbf7c05ba520856505bf90e3e45eb8942d709c9
Aykut Akin <aykutakin90@gmail.com>
2013-01-03 09:27:45 +0200

    Changes regex and hardcoded csv to seperator

--- idp/parser/moviesparser.py
+++ idp/parser/moviesparser.py
@@ -4,14 +4,14 @@ import logging
 
 class MoviesParser(BaseParser):
     """
-    RegExp: /(.*?) (\(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)(\(.+?\))\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)/gm
-    pattern: (.*?) (\(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)(\(.+?\))\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)
+    RegExp: /((.*? \(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)\s?(\(.+?\))\})?\s?(\{\{SUSPENDED\}\})?)\s*(.*$)/gm
+    pattern: ((.*? \(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)\s?(\(.+?\))\})?\s?(\{\{SUSPENDED\}\})?)\s*(.*$)
     flags: gm
     6 capturing groups: 
-        group 1: (.*?)                               title
-        group 2: (\(\S{4,}\))                        year
+        group 1: #TITLE (UNIQUE KEY)
+        group 2: (.*? \(\S{4,}\))                    movie name + year
         group 3: (\(.+\))                            type ex:(TV)
-        group 4: (\{(.*?)(\(.+?\))\})                series info ex: {Ally Abroad (#3.1)}
+        group 4: (\{(.*?)\s?(\(.+?\))\})             series info ex: {Ally Abroad (#3.1)}
         group 5: (.*?)                               episode name ex: Ally Abroad
         group 6: (\(.+?\))                           episode number ex: (#3.1)
         group 7: (\{\{SUSPENDED\}\})                 is suspended?
@@ -19,7 +19,7 @@ class MoviesParser(BaseParser):
     """
   
     # properties
-    baseMatcherPattern = "(.*?) (\(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)(\(.+?\))\})?\s*(\{\{SUSPENDED\}\})?\s*(.*$)"
+    baseMatcherPattern = "((.*? \(\S{4,}\))\s?(\(.+\))?\s?(\{(.*?)\s?(\(.+?\))\})?\s?(\{\{SUSPENDED\}\})?)\s*(.*$)"
     inputFileName = "movies.list"
     numberOfLinesToBeSkipped = 15
 
@@ -47,7 +47,7 @@ class MoviesParser(BaseParser):
             isMatch = matcher.match(self.baseMatcherPattern)
 
             if(isMatch):
-                outputFile.write(matcher.group(1) + "," + matcher.group(2) + "," + matcher.group(3) + "," + matcher.group(5) + "," + matcher.group(6) + "," + matcher.group(7) + "," + matcher.group(8) + "\n")
+                outputFile.write(matcher.group(1).strip() + self.seperator + matcher.group(2) + self.seperator + matcher.group(3) + self.seperator + matcher.group(5) + self.seperator + matcher.group(6) + self.seperator + matcher.group(7) + self.seperator + matcher.group(8) + "\n")
             else:
                 logging.critical("This line is fucked up: " + line)
                 fuckedUpCount += 1
